485a4fe8cedcf944d20c2e123aef36ec6e72106e
mactat <maciektatarski@gmail.com>
2022-10-02 08:39:17 +0000

    comments

--- frontend/app.py
+++ frontend/app.py
@@ -6,6 +6,33 @@ CORS(app)
 @app.route("/")
 def serve():
     return render_template("visualize.html")
+# TODO:
+# - dynamic map-names fetching from backend
+# - start and stop for each agent stored in backend
+# - agent state (idle, computing, dead) in backend
+# - map creator
+# - forcing agent start end in map service
+# - map service stores agent start and end positions
+# - map service can spawn agent on EXISTING map
+# - droping agents start end on the map
+# - fix single agent calculate to return real paths, map them to time
+# - algorithm selection -> tab should look like this:
+#   | Select algorithm |
+#   --------------------
+#   | Single agent     |
+#   |******************|
+#   | A*               |
+#   | Dijkstra         |
+#   | BFS              |
+#   | Multi agent      |
+#   |******************|
+#   | Sequence agent   |
+#   |------------------|
+#   | CA*              |
+#   | Cloud guided     |
+#   |------------------|
+#   | CA*              |
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8888, debug=True)
