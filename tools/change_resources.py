bc85c8032d798d21086ec80ea3b89e70704a072b
Maciej Tatarski <maciej.tatarski@motorolasolutions.com>
2022-12-08 13:22:47 +0100

    res to str

--- tools/change_resources.py
+++ tools/change_resources.py
@@ -4,9 +4,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Change resources')
 parser.add_argument('--config', type=str, default='docker-swarm.yml', help='config file')
-parser.add_argument('--limit-memory', type=int, default=512, help='memory')
+parser.add_argument('--limit-memory', type=str, default="512M", help='memory')
 parser.add_argument('--limit-cpu', type=float, default=0.5, help='cpu')
-parser.add_argument('--reservation-memory', type=int, default=128, help='memory')
+parser.add_argument('--reservation-memory', type=str, default="128M", help='memory')
 parser.add_argument('--reservation-cpu', type=float, default=0.25, help='cpu')
 args = parser.parse_args()
 
@@ -14,9 +14,9 @@ args = parser.parse_args()
 with open(args.config, 'r+') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
     # replace values
-    config['services']['agent']['deploy']['resources']['limits']['memory'] = f"{args.limit_memory}M"
+    config['services']['agent']['deploy']['resources']['limits']['memory'] = f"{args.limit_memory}"
     config['services']['agent']['deploy']['resources']['limits']['cpu'] = args.limit_cpu
-    config['services']['agent']['deploy']['resources']['reservations']['memory'] = f"{args.reservation_memory}M"
+    config['services']['agent']['deploy']['resources']['reservations']['memory'] = f"{args.reservation_memory}"
     config['services']['agent']['deploy']['resources']['reservations']['cpu'] = args.reservation_cpu
     # write back to file
     f.seek(0)
